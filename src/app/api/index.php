38212a1bdcd57aeb979688342487058e49fd4b28
Mokhtaar <mokh.first@gmail.com>
2023-07-20 15:12:31 +0300

    updates

--- src/app/api/index.php
+++ src/app/api/index.php
@@ -7,7 +7,6 @@ header("Access-Control-Allow-Methods: *");
 
 include 'DB.php';
 include 'Product.php';
-include 'ProductRepository.php';
 include 'Book.php';
 include 'DVDdisc.php';
 include 'Furniture.php';
@@ -20,17 +19,13 @@ function handleRequest()
     switch ($method) {
 
         case "GET":
-            #$product = new ProductRepository(new MySQLDatabase());
-            // $productList = new ProductRepository();
-            // $productList->setConnection(new MySQLDatabase());
-            // $productList->getProducts();
             $productList = new MySQLDatabase();
             $products = $productList->getAllProducts();
             echo json_encode($products);
             break;
 
         case "POST":
-            $data = json_decode(file_get_contents('php://input'), true); //true to convert it to an array
+            $data = json_decode(file_get_contents('php://input'), true); 
             $productType = $data['type'];
             $product = new $productType(new MySQLDatabase());
             $product->addProduct($data);
