dfad9e888094c963d63e3cc311946aca112e40f8
Mokhtaar <mokh.first@gmail.com>
2023-08-05 17:11:45 +0300

    updates

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full fixed bottom-0">
-
+    <div>
       <div className="px-5 sm:px-12 mx-auto">
         <div className="border border-black"></div>
-        <h1 className="text-center py-10">Scandiweb Test assignment</h1>
+        <h1 className="text-center py-8">Scandiweb Test assignment</h1>
       </div>
     </div>
   );
